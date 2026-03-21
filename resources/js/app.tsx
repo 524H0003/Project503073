4cf197b80f18ca96d8280535ea0a7763b95218fa
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-03-21 08:57:02 +0000

    chore: update

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,19 +1,26 @@
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ComponentType } from "react";
 
 createInertiaApp({
-  // Sử dụng resolvePageComponent để hỗ trợ code-splitting và TS tốt hơn
-  resolve: (name) =>
+  resolve: (name: string) =>
     resolvePageComponent(
       `./Pages/${name}.tsx`,
       import.meta.glob("./Pages/**/*.tsx"),
     ),
-  setup({ el, App, props }) {
-    // el là HTMLElement, App là Component, props là dữ liệu từ server
+  setup({
+    el,
+    App,
+    props,
+  }: {
+    el: HTMLElement;
+    App: ComponentType;
+    props: object;
+  }) {
     createRoot(el).render(<App {...props} />);
   },
   progress: {
-    color: "#4B5563", // Thanh loading nhỏ ở trên cùng khi chuyển trang
+    color: "#4B5563",
   },
 });
